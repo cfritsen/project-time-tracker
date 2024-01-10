71455877d508885a2ee776313017dc6b613c3745
cfritsen <chrisfritsen@gmail.com>
2024-01-10 15:01:03 -0800

    Added styles to TimeCardList.

--- src/components/TimeCardList.js
+++ src/components/TimeCardList.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import TimeCard, {GetHours} from './TimeCard'
 import './TimeCardList.css'
 
 //-------------------------------------------
 export default function TimeCardList(props) {
     const {timeEntryList} = props;
-    console.log(timeEntryList)
 
-    const sumHours = timeEntryHours => {
-        for (let entry in timeEntryHours){
-            
-        }
-    }
+    const [totalHours, setTotalHours] = useState(0)
+
+    useEffect(() => {
+        setTotalHours(sumHours(timeEntryList))
+    }, [timeEntryList])
 
     return(
-        <div class='timecard-list'>
-            <span>Total: <GetHours /></span>
+        <div className='timecard-list'>
+            <span className='total'>Total: <GetHours timeEntry={totalHours}/></span>
             {timeEntryList.map((timeEntry, index) => (<TimeCard key={index} startTime={timeEntry.startTime} endTime={timeEntry.endTime} timeEntry={timeEntry.timeEntry} />))}
         </div>
     )
 }
 //--------------------------------------------
+
+
+const sumHours = timeEntryHours => {
+    let sum = 0;
+    for (let entry = 0; entry < timeEntryHours.length; entry++){
+        sum += timeEntryHours[entry].timeEntry;
+    }
+    return sum;
+}
